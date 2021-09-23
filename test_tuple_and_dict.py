dc3a980333fcdaa878a3a76222c4eecd3a6f9225
Artem Veselov <artem.veselov@open-com.ru>
2021-09-23 20:59:15 +0300

    fix adding new key-value in dict

--- test_tuple_and_dict.py
+++ test_tuple_and_dict.py
@@ -9,6 +9,7 @@ def test_check_item_in_tuple():
     assert x in fruits
 
 
+# negative test
 def test_try_append_item_in_tuple():
     try:
         fruits.append('watermelon')
@@ -24,16 +25,18 @@ def test_letter_a_in_item():
 
 
 def test_check_item_count():
-    assert fruits_count['apple'] == 5
+    expected_count = 17
+    assert fruits_count['apple'] == expected_count
 
 
 def test_append_new_fruit():
     new_fruit = {'kiwi': 7}
     fruits_count.update(new_fruit)
     assert 'kiwi' in fruits_count
+    fruits_count.popitem()
 
 
-@pytest.mark.parametrize("min_value", [-10, 0, 3, 5, 6, 7, 8, 9])
+@pytest.mark.parametrize("min_value", [0, 3, 5, 6, 7, 8, 9])
 def test_check_fruits_count(min_value):
     for f in fruits_count.values():
         assert f >= min_value
